a2285c9234385ec0f4bc82a53be9fe48343af745
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-01-21 11:55:02 +0100

    record view page fixed

--- class/record.class.php
+++ class/record.class.php
@@ -3,7 +3,7 @@ require ("global.class.php");
 class Record extends Generic{
   public $id;
   function __construct($id){
-    $this->id=499;
+    $this->id=$id;
   }
   public function info(){
     return $this->simple("select * from recordview where id = ".$this->id.";");
