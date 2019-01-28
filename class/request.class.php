057839bbb9eb103dfb32f807c21fd0d45cc65de0
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-01-28 08:02:16 +0100

    accept request functions

--- class/request.class.php
+++ class/request.class.php
@@ -2,7 +2,7 @@
 require("user.class.php");
 class Request extends User{
   private $usr;
-  function __construct(int $id){ $this->usr = $id; }
+  function __construct($id){ $this->usr = $id; }
 
   public function usrInfo(){
     return $this->simple("select a.id, a.last_name, a.first_name, a.cell, a.description, a.address, a.email, b.data::date as data, b.data::time as hour from addr_book a, request b where b.address = a.id and a.id=".$this->usr.";");
