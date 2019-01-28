bf1e6e7553b5ab6b37cb687ba75ab7b9001e66ad
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-01-28 12:56:37 +0100

    fixed bug

---
+++ db/190128.sql
@@ -0,0 +1,8 @@
+select audit.audit_table('addr_book');
+select audit.audit_table('author');
+select audit.audit_table('find');
+select audit.audit_table('localization');
+select audit.audit_table('login_request');
+select audit.audit_table('record');
+select audit.audit_table('request');
+select audit.audit_table('usr');
