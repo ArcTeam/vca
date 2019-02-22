e4bda6bba85747f47f9b2886a03cb3ee48b2fdd9
Giuseppe Naponiello <beppenapo@arc-teamcom>
2019-02-22 09:28:41 +0100

    pulizia file system

--- db/181211.sql
+++
@@ -1,1 +0,0 @@
-create table request(address integer primary key references addr_book(id) on delete cascade, data timestamp with time zone not null default now());
--- db/190126.sql
+++
@@ -1,4 +0,0 @@
-alter table usr add column password text;
-update usr set password = crypt('alfaomega', gen_salt('bf', 8)) where id = 9;
-alter table usr drop column pwd, drop column salt;
-alter table usr rename column password to pwd;
