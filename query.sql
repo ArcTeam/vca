f4e14f3df1361974d81da7d16668a28d50cdad14
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-03-04 13:19:41 +0100

    mappa poi

--- query.sql
+++ query.sql
@@ -1,21 +1,41 @@
-SELECT row_to_json(poi.*) AS geometrie
-FROM (
-  SELECT 'FeatureCollection'::text AS type, array_to_json(array_agg(features.*)) AS features
-  FROM (
-    SELECT 'Feature'::text AS type, st_asgeojson(localization.geom)::json AS geometry, row_to_json(prop.*) AS properties
-    FROM localization
-    JOIN (
-      SELECT state.name state, land.name land, municipality.name municipality, localization.lon, localization.lat, record.id, record.name, cronostartlist.definition cronostart, cronoendlist.definition cronoend, type.type
-      FROM record
-      left join localization on localization.record = record.id
-      left join geodati.state state on localization.state = state.id
-      left join geodati.land land on localization.land = land.id
-      left join geodati.municipality municipality on localization.municipality = municipality.id
-      left join chronology cronostart on cronostart.record = record.id
-      left join chronology cronoend on cronoend.record = record.id
-      left join list.chronology cronostartlist on cronostart.cronostart = cronostartlist.id
-      left join list.chronology cronoendlist on cronoend.cronoend = cronoendlist.id
-      left join list.recordtype type on record.type = type.id
-    ) prop ON localization.record = prop.id
-  ) features
-) poi;
+﻿SELECT 
+  record.id, 
+  state.name AS state, 
+  land.name AS land, 
+  municipality.name AS municipality, 
+  localization.toponym, 
+  localization.address, 
+  localization.lon, 
+  localization.lat, 
+  record.name, 
+  cronostart.definition AS cronostart, 
+  cronoend.definition AS cronoend, 
+  record.info, 
+  addr_book.last_name, 
+  recordtype.type, 
+  record.tag, 
+  record.relatedrecord, 
+  chronology.period, 
+  record.biblio, 
+  record.data
+FROM 
+  public.record, 
+  public.localization, 
+  list.recordtype, 
+  geodati.state, 
+  geodati.land, 
+  geodati.municipality, 
+  public.addr_book, 
+  public.chronology, 
+  list.chronology cronostart, 
+  list.chronology cronoend
+WHERE 
+  record.type = recordtype.id AND
+  record.compiler = addr_book.id AND
+  localization.record = record.id AND
+  localization.municipality = municipality.id AND
+  localization.land = land.id AND
+  localization.state = state.id AND
+  chronology.record = record.id AND
+  chronology.cronostart = cronostart.id AND
+  chronology.cronoend = cronoend.id;
